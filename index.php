2e13a3bc292ba715e0c8c596b423a2acae241059
Shahan Butt <shahanbutt0@gmail.com>
2020-12-09 23:47:52 +0400

    https

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <head>
   <title>MemePower</title>
-  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
   <style>
     .wrapper {
       background-color: #fff;
